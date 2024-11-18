8118dc25c9a1145080b094bb05868538add4c5db
Saska <a.revilakova@protonmail.com>
2024-11-18 15:58:48 +0100

    implementation of integration tests for user photos

--- src/test/java/com/asos/reservationSystem/integrationTests/UserControllerIntegrationTests.java
+++ src/test/java/com/asos/reservationSystem/integrationTests/UserControllerIntegrationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -15,6 +16,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 @SpringBootTest
@@ -51,7 +54,7 @@ public class UserControllerIntegrationTests {
 
         mockMvc.perform(
                         MockMvcRequestBuilders.get("/user")
-                                .header("Authorization", "Bearer " + token) // Bearer token
+                                .header("Authorization", "Bearer " + token)
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -63,5 +66,215 @@ public class UserControllerIntegrationTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.phoneNumber").value("+421949111111"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.role").value("STUDENT"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.description").doesNotExist());
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/auth/logout")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
     }
+
+    @Test
+    public void testUploadPhotoSuccessfullyUploadsPhoto() throws Exception {
+        String loginRequest = objectMapper.writeValueAsString(Map.of(
+                "email", "petra.kovacova@student.com",
+                "password", "student"
+        ));
+
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequest)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andReturn();
+
+        String responseContent = loginResult.getResponse().getContentAsString();
+        String token = objectMapper.readTree(responseContent).get("access_token").asText();
+
+        byte[] imageContent = Files.readAllBytes(
+                Path.of("src/test/java/com/asos/reservationSystem/unitTests/graf1.png"));
+
+        MockMultipartFile photo = new MockMultipartFile(
+                "photo",
+                "test-photo.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                imageContent
+        );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.multipart("/photo")
+                                .file(photo)
+                                .header("Authorization", "Bearer " + token)
+                                .contentType(MediaType.MULTIPART_FORM_DATA)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Image uploaded successfully."));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/auth/logout")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+
+    @Test
+    public void testGetPhotoSuccessfullyReturnsPhoto() throws Exception {
+        String loginRequest = objectMapper.writeValueAsString(Map.of(
+                "email", "student@student.com",
+                "password", "student"
+        ));
+
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequest)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andReturn();
+
+        String responseContent = loginResult.getResponse().getContentAsString();
+        String token = objectMapper.readTree(responseContent).get("access_token").asText();
+
+        Long userId = 2L;
+
+        byte[] expectedPhotoBytes = Files.readAllBytes(
+                Path.of("src/test/java/com/asos/reservationSystem/unitTests/graf1.png"));
+
+        MockMultipartFile photo = new MockMultipartFile(
+                "photo",
+                "test-photo.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                expectedPhotoBytes
+        );
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.multipart("/photo")
+                        .file(photo)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+        );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/photo/{id}", userId)
+                                .header("Authorization", "Bearer " + token)
+                                .accept(MediaType.IMAGE_JPEG)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.IMAGE_JPEG))
+                .andExpect(MockMvcResultMatchers.content().bytes(expectedPhotoBytes));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/auth/logout")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+
+    @Test
+    public void testGetPhotoReturnsNotFoundBecausePictureIsNull() throws Exception {
+        String loginRequest = objectMapper.writeValueAsString(Map.of(
+                "email", "petra.kovacova@student.com",
+                "password", "student"
+        ));
+
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequest)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andReturn();
+
+        String responseContent = loginResult.getResponse().getContentAsString();
+        String token = objectMapper.readTree(responseContent).get("access_token").asText();
+
+        Long userId = 3L;
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/photo/{id}", userId)
+                                .header("Authorization", "Bearer " + token)
+                                .accept(MediaType.IMAGE_JPEG)
+                )
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.IMAGE_JPEG))
+                .andExpect(MockMvcResultMatchers.content().string("Image not found for user."));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/auth/logout")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+
+    @Test
+    public void testGetPhotoReturnsNotFoundBecauseUserDoesNotExist() throws Exception {
+        String loginRequest = objectMapper.writeValueAsString(Map.of(
+                "email", "student@student.com",
+                "password", "student"
+        ));
+
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequest)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andReturn();
+
+        String responseContent = loginResult.getResponse().getContentAsString();
+        String token = objectMapper.readTree(responseContent).get("access_token").asText();
+
+        Long userId = 10L;
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/photo/{id}", userId)
+                                .header("Authorization", "Bearer " + token)
+                                .accept(MediaType.IMAGE_JPEG)
+                )
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.IMAGE_JPEG))
+                .andExpect(MockMvcResultMatchers.content().string("User does not exist."));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/auth/logout")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+
+    @Test
+    public void testDeletePhotoSuccessfullyRemovesPhoto() throws Exception {
+        String loginRequest = objectMapper.writeValueAsString(Map.of(
+                "email", "petra.kovacova@student.com",
+                "password", "student"
+        ));
+
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequest)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andReturn();
+
+        String responseContent = loginResult.getResponse().getContentAsString();
+        String token = objectMapper.readTree(responseContent).get("access_token").asText();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.delete("/photo")
+                                .header("Authorization", "Bearer " + token)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Profile photo removed successfully."));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/auth/logout")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+
+
 }
